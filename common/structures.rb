260cc6270ce9fa6859ae49b898914030245ba79c
Olivier <olivier@veilleperso.com>
2011-02-19 22:14:48 +0100

    * Fonction pour inspecter les différentes classes * On ajoute 1 de confiance aux version tag par rapport au version notag

--- common/structures.rb
+++ common/structures.rb
@@ -2,8 +2,8 @@
 # Constantes
 # ------------------------------------------
 BROWSER = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-  agent.follow_meta_refresh = false
+	agent.user_agent_alias = 'Mac Safari'
+	agent.follow_meta_refresh = false
 }
 
 # ------------------------------------------
@@ -11,13 +11,13 @@ BROWSER = Mechanize.new { |agent|
 # ------------------------------------------
 
 class CommonStruct
-  # on met en place la possibilité d'initialiser une structure via
-  # Structure.new(:key1 => val1, :key2 => val2, ...)
-  def initialize(options = {})
-    options.each do |k,v|
-      self.send("#{k}=", v) if self.respond_to?(k)
-    end
-  end
+	# on met en place la possibilité d'initialiser une structure via
+	# Structure.new(:key1 => val1, :key2 => val2, ...)
+	def initialize(options = {})
+		options.each do |k,v|
+			self.send("#{k}=", v) if self.respond_to?(k)
+		end
+	end
 end
 
 # ------------------------------------------
@@ -27,80 +27,84 @@ end
 
 # Structure d'insertion dans la liste
 class Ligne < CommonStruct
-  attr_accessor :fichier, :date, :conf, :comment
-  attr_accessor :serie, :saison, :episode, :team, :format, :source, :provider, :titre, :infos
-  attr_accessor :repTarget, :fileTarget
-  attr_accessor :forom, :seriessub, :podnapisi, :tvsubs, :tvsubtitles, :local, :mysource, :soustitreseu
-  attr_accessor :status, :candidats
+	attr_accessor :fichier, :date, :conf, :comment
+	attr_accessor :serie, :saison, :episode, :team, :format, :source, :provider, :titre, :infos
+	attr_accessor :repTarget, :fileTarget
+	attr_accessor :forom, :seriessub, :podnapisi, :tvsubs, :tvsubtitles, :local, :mysource, :soustitreseu
+	attr_accessor :status, :candidats
+	
+	def to_s
+		"<Ligne serie:#{serie} - saison: #{saison} - episode: #{episode} - team:#{team} - format: #{format} - high_def: #{high_def}>"
+	end
 end
 
 # Structure de gestion des sous titres trouvés (candidats)
 class WebSub < CommonStruct
-  attr_accessor :fichier, :date, :lien, :confiant, :source, :referer
+	attr_accessor :fichier, :date, :lien, :confiant, :source, :referer
 
-  def to_s
-    "#{fichier} - #{date} - #{lien} - #{confiant}"
-  end
+	def to_s
+		"<WebSub fichier:{fichier} - date:#{date} - lien:#{lien} - confiant:#{confiant}>"
+	end
 end
 
 # Structure d'insertion dans la liste des séries
 class Series < CommonStruct
-  attr_accessor :nom, :image, :idtvdb
-  
-  def to_s
-    "#{nom} - #{idtvdb}"
-  end
+	attr_accessor :nom, :image, :idtvdb
+	
+	def to_s
+		"<Series nom:#{nom} - image:#{idtvdb}>"
+	end
 end
 
 # Structure d'insertion dans la Librairie
 class Library < CommonStruct
-  attr_accessor :serie, :saison, :status, :URLTVdb, :nbepisodes, :firstep, :lastep, :image, :episodes
+	attr_accessor :serie, :saison, :status, :URLTVdb, :nbepisodes, :firstep, :lastep, :image, :episodes
 
-  def to_s
-    "#{serie} - #{saison} - #{status} - #{nbepisodes}"
-  end
+	def to_s
+		"<Library serie:#{serie} - saison:#{saison} - status:#{status} - nbepisodes:#{nbepisodes}>"
+	end
 end
 
 # Structure d'insertion dans les stats
 class Stats < CommonStruct
-  attr_accessor :source, :image, :search, :stime, :sfound, :smark, :process, :ptime, :pratio, :pauto
-  attr_accessor :TimeSearch, :TimeProcess, :NbFound, :TotalMarks, :NbAuto
+	attr_accessor :source, :image, :search, :stime, :sfound, :smark, :process, :ptime, :pratio, :pauto
+	attr_accessor :TimeSearch, :TimeProcess, :NbFound, :TotalMarks, :NbAuto
 end
 
 # Structure d'insertion dans les sources
 class Sources < CommonStruct
-  attr_accessor :source, :image, :active, :rank
+	attr_accessor :source, :image, :active, :rank
 
-  def to_s
-    "#{source} - #{active} - #{rank}"
-  end
+	def to_s
+		"<Sources source:#{source} - active:#{active} - rank:#{rank}>"
+	end
 end
 
 # Structure de gestion pour les infos d'une saison
 class InfosSaison < CommonStruct
-  attr_accessor :episode, :titre, :diffusion, :telecharge, :soustitre
-  
-  def to_s
-    "#{episode} - #{titre} - #{diffusion}"
-  end
+	attr_accessor :episode, :titre, :diffusion, :telecharge, :soustitre
+	
+	def to_s
+		"<InfosSaison episode:#{episode} - titre:#{titre} - diffusion:#{diffusion}>"
+	end
 end
 
 
 class Hash
 
-  def deep_merge(hash)
-    target = dup
+	def deep_merge(hash)
+		target = dup
 
-    hash.keys.each do |key|
-      if hash[key].is_a? Hash and self[key].is_a? Hash
-        target[key] = target[key].deep_merge(hash[key])
-        next
-      end
+		hash.keys.each do |key|
+			if hash[key].is_a? Hash and self[key].is_a? Hash
+				target[key] = target[key].deep_merge(hash[key])
+				next
+			end
 
-      target[key] = hash[key]
-    end
+			target[key] = hash[key]
+		end
 
-    target
-  end
+		target
+	end
 
 end
--- plugin/sous_titres_eu.rb
+++ plugin/sous_titres_eu.rb
@@ -1,62 +1,62 @@
 class Plugin::SousTitresEU < Plugin::Base
-  INDEX = 3
-  ICONE = 'soustitreseu.png'
-  NAME = 'soustitreseu'
+	INDEX = 3
+	ICONE = 'soustitreseu.png'
+	NAME = 'soustitreseu'
 
-  def get_from_source
-    item = current.candidats[idx_candidat]
-    FileCache.get_zip("http://www.sous-titres.eu/series/#{item.lien}", item.fichier)
-  end
+	def get_from_source
+		item = current.candidats[idx_candidat]
+		FileCache.get_zip("http://www.sous-titres.eu/series/#{item.lien}", item.fichier)
+	end
 
-  def do_search
-    monURL = "http://www.sous-titres.eu/series/#{current.serie.downcase.gsub(/ /, '_')}.html"
-    rec_saison = sprintf("%s\.S0?%s", current.serie.downcase.gsub(/ /, '.'), current.saison, current.saison)
+	def do_search
+		monURL = "http://www.sous-titres.eu/series/#{current.serie.downcase.gsub(/ /, '_')}.html"
+		rec_saison = sprintf("%s\.S0?%s", current.serie.downcase.gsub(/ /, '.'), current.saison, current.saison)
 
-    doc = FileCache.get_html(monURL)
-    doc.search("div.saison tr").collect do |k|
-      blk = k.at("td.filename a")
-      next if blk.nil?
-      
-      fichierCible = blk.text
-      vers = blk.attr("href").to_s
-      rel = k.search("td.update").text.to_s
+		doc = FileCache.get_html(monURL)
+		doc.search("div.saison tr").collect do |k|
+			blk = k.at("td.filename a")
+			next if blk.nil?
+			
+			fichierCible = blk.text
+			vers = blk.attr("href").to_s
+			rel = k.search("td.update").text.to_s
 
-      # Cas du zip par episode
-      if valid_episode?(fichierCible, current.saison, current.episode)
-        # Récupération du zip
-        path = FileCache.get_file("http://www.sous-titres.eu/series/#{vers}", :zip => true)
+			# Cas du zip par episode
+			if self.class.valid_episode?(fichierCible, current.saison, current.episode)
+				# Récupération du zip
+				path = FileCache.get_file("http://www.sous-titres.eu/series/#{vers}", :zip => true)
 
-        # Exploration du zip
-        `zipinfo -1 #{path}`.collect do |entry|
-          if entry.to_s.match(/\.srt/im) && !entry.to_s.match(/\.(EN|VO)\./im)
-            new_ligne = WebSub.new
-            new_ligne.fichier = entry.to_s.strip
-            new_ligne.date = rel.to_s
-            new_ligne.lien = vers.to_s
-            new_ligne.referer = monURL
-            new_ligne
-          end
-        end
-        # Cas du zip par saison
-      elsif k.text.downcase.match(/#{rec_saison}/im)
-        # on zappe les zips de saison uniquement en anglais
-        next if fichierCible.match(/\.(EN|VO)\./)
-        # Récupération du zip
-        path = FileCache.get_file("http://www.sous-titres.eu/series/#{vers}", :zip => true)
+				# Exploration du zip
+				`zipinfo -1 #{path}`.collect do |entry|
+					if entry.to_s.match(/\.srt/im) && !entry.to_s.match(/\.(EN|VO)\./im)
+						new_ligne = WebSub.new
+						new_ligne.fichier = entry.to_s.strip
+						new_ligne.date = rel.to_s
+						new_ligne.lien = vers.to_s
+						new_ligne.referer = monURL
+						new_ligne
+					end
+				end
+				# Cas du zip par saison
+			elsif k.text.downcase.match(/#{rec_saison}/im)
+				# on zappe les zips de saison uniquement en anglais
+				next if fichierCible.match(/\.(EN|VO)\./)
+				# Récupération du zip
+				path = FileCache.get_file("http://www.sous-titres.eu/series/#{vers}", :zip => true)
 
-        # Exploration du zip
-        `zipinfo -1 #{path}`.collect do |entry|
-          next unless valid_episode?(entry, current.saison, current.episode)
-          new_ligne = WebSub.new
-          new_ligne.fichier = entry.to_s.strip
-          new_ligne.date = rel.to_s
-          new_ligne.lien = vers.to_s
-          new_ligne.confiant = get_confiance(new_ligne.fichier.downcase)
-          new_ligne.source = "SousTitresEU"
-          new_ligne.referer = monURL
-          new_ligne
-        end
-      end
-    end
-  end
+				# Exploration du zip
+				`zipinfo -1 #{path}`.collect do |entry|
+					next unless self.class.valid_episode?(entry, current.saison, current.episode)
+					new_ligne = WebSub.new
+					new_ligne.fichier = entry.to_s.strip
+					new_ligne.date = rel.to_s
+					new_ligne.lien = vers.to_s
+					new_ligne.confiant = get_confiance(new_ligne.fichier.downcase)
+					new_ligne.source = "SousTitresEU"
+					new_ligne.referer = monURL
+					new_ligne
+				end
+			end
+		end
+	end
 end
