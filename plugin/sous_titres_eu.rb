94568d8295bf5324346ccdfbae335e6348a90fe2
Olivier <olivier@veilleperso.com>
2011-02-19 17:29:17 +0100

    * DRY du code pour avoir une fonction valid_episode? qui teste la validité d'un nom d'épisode par rapport aux différents formats * Utilisation de cette fonction dans soustitreeu

--- plugin/sous_titres_eu.rb
+++ plugin/sous_titres_eu.rb
@@ -10,19 +10,23 @@ class Plugin::SousTitresEU < Plugin::Base
 
   def do_search
     monURL = "http://www.sous-titres.eu/series/#{current.serie.downcase.gsub(/ /, '_')}.html"
-    rec1 = sprintf("%dx%02d", current.saison, current.episode)
-    rec2 = sprintf("%s.s%02d", current.serie.downcase.gsub(/ /, '.'), current.saison)
-    rec3 = sprintf("S%02dE%02d", current.saison, current.episode)
+    rec1 = sprintf("%dx0?%s", current.saison, current.episode)
+    rec2 = sprintf("%s\.S0?%s", current.serie.downcase.gsub(/ /, '.'), current.saison, current.saison)
+    rec3 = sprintf("S0?%sE0%s", current.saison, current.episode)
+
+    $stderr.puts "REC: #{rec1} - #{rec2} - #{rec3}"
 
     doc = FileCache.get_html(monURL)
     doc.search("div.saison tr").collect do |k|
-      # Cas du zip par episode
-      if k.text.downcase.match(rec1)
-        blk = k.at("td.filename a")
-        fichierCible = blk.text
-        vers = blk.attr("href").to_s
-        rel = k.search("td.update").text.to_s
+      blk = k.at("td.filename a")
+      next if blk.nil?
+      
+      fichierCible = blk.text
+      vers = blk.attr("href").to_s
+      rel = k.search("td.update").text.to_s
 
+      # Cas du zip par episode
+      if valid_episode?(fichierCible, current.saison, current.episode)
         # Récupération du zip
         path = FileCache.get_file("http://www.sous-titres.eu/series/#{vers}", :zip => true)
 
@@ -36,30 +40,25 @@ class Plugin::SousTitresEU < Plugin::Base
             new_ligne.referer = monURL
             new_ligne
           end
-        end.flatten
-      # Cas du zip par saison
-      elsif k.text.downcase.match(rec2)
-        blk = k.at("td.filename a")
-        fichierCible = blk.text
-        vers = blk.attr("href").to_s
-        rel = k.search("td.update").text.to_s
-
+        end
+        # Cas du zip par saison
+      elsif k.text.downcase.match(/#{rec2}/im)
+        # on zappe les zips de saison uniquement en anglais
+        next if fichierCible.match(/\.(EN|VO)\./)
         # Récupération du zip
         path = FileCache.get_file("http://www.sous-titres.eu/series/#{vers}", :zip => true)
 
         # Exploration du zip
         `zipinfo -1 #{path}`.collect do |entry|
-          if entry.to_s.match(/\.srt/im) && entry.to_s.match(rec3) && !entry.to_s.match(/\.(EN|VO)\./im)
-            $stderr.puts "... match for #{entry}!"
-            new_ligne = WebSub.new
-            new_ligne.fichier = entry.to_s.strip
-            new_ligne.date = rel.to_s
-            new_ligne.lien = vers.to_s
-            new_ligne.confiant = get_confiance(new_ligne.fichier.downcase)
-            new_ligne.source = "SousTitresEU"
-            new_ligne.referer = monURL
-            new_ligne
-          end
+          next unless valid_episode?(entry, current.saison, current.episode)
+          new_ligne = WebSub.new
+          new_ligne.fichier = entry.to_s.strip
+          new_ligne.date = rel.to_s
+          new_ligne.lien = vers.to_s
+          new_ligne.confiant = get_confiance(new_ligne.fichier.downcase)
+          new_ligne.source = "SousTitresEU"
+          new_ligne.referer = monURL
+          new_ligne
         end
       end
     end
